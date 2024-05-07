2613f7829b3dd8119f7606b0edf00199ef36463c
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2024-05-07 14:25:44 -0700

    Working on Animation

--- Final Game Spring 2024/Assets/Scripts/PlayerController.cs
+++ Final Game Spring 2024/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   [Header("Assigned Components")]
+    [Header("Assigned Components")]
     [SerializeField] private Animator _animator;
     [SerializeField] private CharacterController _characterController;
 
     [Header("Player Movement Values")]
     public float maximumSpeed;
     public float rotationSpeed;
+    [Header("Jumping Values")]
     public float jumpForce;
     public float jumpButtonGracePeriod;
     private float _yForce;
     private float originalStepOffset;
     private float? _lastGroundedTime;
     private float? _jumpButtonPressedTime;
+    [SerializeField] private bool _isGrounded;
+    [SerializeField] private bool _isJumping;
 
     // Start is called before the first frame update
     void Start()
@@ -58,10 +61,17 @@ public class PlayerController : MonoBehaviour
         {
             _characterController.stepOffset = originalStepOffset;
             _yForce = -0.5f;
+            _animator.SetBool("IsGrounded", true);
+            _isGrounded = true;
+            _animator.SetBool("IsJumping", false);
+            _isJumping = false;
+            //_animator.SetBool("IsFalling", false);
 
             if (Time.time - _jumpButtonPressedTime <= jumpButtonGracePeriod)
             {
                 _yForce = jumpForce;
+                _animator.SetBool("IsJumping", true);
+                _isJumping = true;
                 _jumpButtonPressedTime = null;
                 _lastGroundedTime = null;
             }
@@ -69,6 +79,8 @@ public class PlayerController : MonoBehaviour
         else
         {
             _characterController.stepOffset = 0;
+            _animator.SetBool("IsGrounded", false);
+            _isGrounded = false;
         }
 
         Vector3 velocity = movementDirection * speed;
@@ -78,9 +90,15 @@ public class PlayerController : MonoBehaviour
 
         if (movementDirection != Vector3.zero)
         {
+            _animator.SetBool("IsMoving", true);
+
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            _animator.SetBool("IsMoving", false);
         }
     } //end Update
 }
