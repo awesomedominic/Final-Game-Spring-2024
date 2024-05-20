7005634f1d7011701c2c1ceafbec8c51a668d311
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2024-05-20 14:17:29 -0700

    working on ending game

--- Final Game Spring 2024/Assets/Scripts/LevelLoader.cs
+++ Final Game Spring 2024/Assets/Scripts/LevelLoader.cs
@@ -2,18 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LevelLoader : MonoBehaviour
 {
     [SerializeField] private CrossFade _crossFade;
     [SerializeField] private Endpoint _endpoint;
     [SerializeField] private Timer _timer;
+    [SerializeField] private string _sceneName;
 
     // Start is called before the first frame update
     void Start()
     {
         _crossFade.FadeOut();
         _timer.StartGameTimer();
+        _sceneName = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
@@ -21,7 +24,13 @@ public class LevelLoader : MonoBehaviour
     {
         if(_endpoint.HasReached() || !_timer.GetGameTimer())
         {
+            GameManager.Instance.CompletedLevel(_sceneName);
             StartCoroutine("EndLevel");
+        }
+
+        if(_endpoint.HasReached() && _sceneName == "Level 3")
+        {
+            StartCoroutine("YouWonGame");
         }
     }
 
@@ -30,5 +39,12 @@ public class LevelLoader : MonoBehaviour
         _crossFade.FadeIn();
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("Level Select");
+    }
+
+    IEnumerator YouWonGame()
+    {
+        _crossFade.FadeIn();
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("You Won");
     }
 }
